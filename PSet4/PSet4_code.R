5c34943be9b3783d8e1c6f3e04d2b2d42e9c2d5a
JingpengHong <jphong0403@outlook.com>
2022-03-05 12:33:12 -0600

    PSet 4 Started

---
+++ PSet4/PSet4_code.R
@@ -0,0 +1,10 @@
+## Machine Learning 2022W
+## Problem Set 4
+## Instructor: Guillaume Pouliot
+## Author: Jingpeng Hong
+## Date: Mar 5, 2022
+
+## Housekeeping ##
+rm(list=ls())
+setwd("/Users/hongjingpeng/Desktop/Machine\ Learning/Machine-Learning-2022W/PSet4")
+
